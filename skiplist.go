ebca99764edb030995c786a158ff15b85c4bdd4a
torshind <marcello.m@yahoo.com>
2023-11-22 16:36:11 +0100

    docs: improve comments

--- skiplist.go
+++ skiplist.go
@@ -69,9 +69,9 @@ func NewSkipList[K cmp.Ordered, V any]() *SkipList[K, V] {
 func (s *SkipList[K, V]) findNode(key K) (*Node[K, V], bool) {
 	x := s.head
 
-	// for each level if I found a greater key I pass to a lower level
-	// if it's inferior I go to the next node
-	// if it's the same I've found the node
+	// For each level, if I find a greater key, I move to a lower level.
+	// If it's inferior, I proceed to the next node.
+	// If it's the same, I've found the node.
 	for i := s.level; i >= 0; i-- {
 		for {
 			if x.next[i] == nil || x.next[i].record.Key > key {
@@ -114,8 +114,8 @@ func (s *SkipList[K, V]) Insert(key K, value V) {
 	x := s.head
 	updates := make([]*Node[K, V], s.level+1)
 
-	// for each level, store in updates the nodes that should be just before the new node
-	// in the loop, search will start from the last found node, no need to restart from head
+	// For each level, store in "updates" the nodes that should precede the new node.
+	// In the loop, the search will start from the last found node; no need to restart from the head.
 	for i := s.level; i >= 0; i-- {
 		for x.next[i] != nil && x.next[i].record.Key < key {
 			x = x.next[i]
@@ -123,13 +123,13 @@ func (s *SkipList[K, V]) Insert(key K, value V) {
 		updates[i] = x
 	}
 
-	// replace value if key already present
+	// Replace value if key already present.
 	if x.next[0] != nil && x.next[0].record.Key == key {
 		x.next[0].record.Value = value
 	} else {
 		newLevel := s.getRandomLevel()
 
-		// add new level
+		// Add new level.
 		if newLevel > s.level {
 			for i := s.level; i < newLevel; i++ {
 				updates = append(updates, s.head)
@@ -139,8 +139,8 @@ func (s *SkipList[K, V]) Insert(key K, value V) {
 
 		newNode := NewNode[K, V](key, value, newLevel)
 
-		// node references swap: the already stored nodes with inferior keys will point to the new node
-		// the new node will point to what the already stored nodes pointed before
+		// Node references swap: nodes with inferior keys that are already stored will now point to the new node.
+		// The new node will point to what the already stored nodes pointed to before.
 		for i := 0; i <= newLevel; i++ {
 			newNode.next[i] = updates[i].next[i]
 			updates[i].next[i] = newNode
@@ -153,8 +153,8 @@ func (s *SkipList[K, V]) Insert(key K, value V) {
 func (s *SkipList[K, V]) Delete(key K) {
 	x := s.head
 
-	// similar to find, but when I found the node I update the reference to the node pointed
-	// by the node to be deleted for each level
+	// Similar to the find operation, but when I locate the node, I update the reference to the node pointed to
+	// by the node to be deleted for each level.
 	for i := s.level; i >= 0; i-- {
 		for {
 			if x.next[i] == nil || x.next[i].record.Key > key {
